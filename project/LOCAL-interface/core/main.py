fbefb40093ef28bb8a1c94a00bd95560c98aef56
Selenia María Medina Hernández <SeleniaMMH@MacBook-Air-de-Selenia-Maria.local>
2021-05-20 17:26:09 +0100

    core update

--- project/LOCAL-interface/core/main.py
+++ project/LOCAL-interface/core/main.py
@@ -9,34 +9,28 @@ from functions.DronPositionSimulationClass import *
 
 #   main program
 # variables.
-ourLat = 0
-ourLon = 0
-ourHei = 0
-dronLat = 0
-dronLon = 0
-dronHei = 0
+ourLat = 0.0
+ourLon = 0.0
+ourHei = 0.0
+dronLat = 0.0
+dronLon = 0.0
+dronHei = 0.0
 posVector = [0, 0, 0]
 posVectorRef = [0, 0, 0]
-rotDeg = 0
-incDeg = 0
+rotDeg = 0.0
+incDeg = 0.0
 portName = "None"
 baudRate = 0
 magValue = [0, 0, 0]
 accValue = [0, 0, 0]
 rotMatrix = [0, 0, 0]
-welcomeMessage = "######################## Antenna Project ######################## \n" \
+dronPosList = []
+welcomeMessage = "\n************************ Antenna Project ************************ \n" \
                  "Team members: \n" \
                  "  ·   MEDINA HERNÁNDEZ, SELENIA MARÍA. \n" \
                  "  ·   BERCIANO RODRÍGUEZ, GEMMA. \n" \
                  "  ·   VEGA GARCÍA, CARLOS. \n" \
-                 "######################## Antenna Project ######################## \n"
-
-# dron positions list
-positionList = []
-positionList.append({'Latitude': 1.1, 'Longitude': 1.2, 'Altitude': 1.3})
-positionList.append({'Latitude': 2.1, 'Longitude': 2.2, 'Altitude': 2.3})
-positionList.append({'Latitude': 3.1, 'Longitude': 3.2, 'Altitude': 3.3})
-positionList.append({'Latitude': 4.1, 'Longitude': 4.2, 'Altitude': 4.3})
+                 "************************ Antenna Project ************************ \n"
 
 # Welcome message.
 print(welcomeMessage)
@@ -44,13 +38,16 @@ print(welcomeMessage)
 # Select USB port and baud rate.
 (portName, baudRate) = SelectUSBPortAndBaudRate()
 
-# Antenna interface object
+# Antenna interface object.
 antenna = AntennaInterface(portName, baudRate)
 
 # multiplex to GPS.
 
 # know our position.
 (ourLat, ourLon) = GetOurPosition(portName, baudRate)
+
+# create DRON positions list
+createDronPositionList(dronPosList, ourLat, ourLon)
 
 # multiplex to arduino.
 
@@ -62,9 +59,6 @@ try:
 except NoFeatures:
     print("ERROR: TIMEOUT")
 
-# format:
-# magValue = [0, 0, 0]
-# accValue = [0, 0, 0]
 accValue = data[0:3]
 print("Accelerometer value: [", accValue[0], ",", accValue[1], ",", accValue[2], "] \n")
 magValue = data[3:6]
@@ -73,14 +67,16 @@ print("Magnetometer value: [", magValue[0], ",", magValue[1], ",", magValue[2], 
 # calculate our rotation matrix.
 rotMatrix = CalculateRotationMatrix(magValue, accValue)
 
-# start position object
-positionObject = PositionSimulation(positionList)
-positionObject.start()
+# position object
+dron = PositionSimulation(dronPosList)
 
 while(True):
 
+    # start DRON position
+    dron.start()
+
     # DRON position
-    (dronLat, dronLon, dronHei) = positionObject.getPosition()
+    (dronLat, dronLon, dronHei) = dron.getPosition()
     print("Current DRON position: (", dronLat, "º,", dronLon, "º,", dronHei, "m)")
 
     # calculate vector position.
@@ -88,15 +84,18 @@ while(True):
 
     # change vector position reference.
     posVectorRef = VectorToVector(posVector, rotMatrix)
-    print("Position vector: [", posVectorRef[0], ",", posVectorRef[1], ",", posVectorRef[2], "]")
+    print("  -   Position vector: [", posVectorRef[0], ",", posVectorRef[1], ",", posVectorRef[2], "]")
 
     # calculate rotation and inclination angles.
     (rotDeg, incDeg) = RotationAndInclination(posVectorRef)
-    print("Rotation angle: ", rotDeg, "º \n Inclination angle: ", incDeg, "º \n")
+    print("  -   Rotation angle: ", rotDeg, "º \n  -   Inclination angle: ", incDeg, "º \n")
 
     # send rotation and inclination angles to arduino (rotDeg, incDeg)
 
     # get arduino response
 
+    # loop delay
+    time.sleep(2)
+
 # stop position object
-# positionObject.stop()
+# dron.stop()
